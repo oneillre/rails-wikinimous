49adea177aa539b14f025a6877b86f3170a3c2fa
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-02-18 16:26:26 +0000

    Some bootstrap styling added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :select_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
@@ -16,23 +15,23 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
     redirect_to articles_path
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
   private
+
+  def select_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :content)
